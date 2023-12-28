fc60552dbd6b256cc8bcc49400723f17a243b920
clever_chinekwe <chinekwe_clever@outlook.com>
2023-12-28 12:54:48 +0100

    comment: added comment for various es6 new features

---
+++ es6.js
@@ -0,0 +1,17 @@
+// JavaScript ES6 for Beginners
+
+// const and let
+
+// arrow functions
+
+// template string
+
+// default parameters
+
+// array destructuring
+
+// object destructuring
+
+// rest operator
+
+// spread operator
