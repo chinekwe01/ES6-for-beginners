13a0249073838f2636079d9ddf0e7c12824ec13e
clever_chinekwe <chinekwe_clever@outlook.com>
2023-12-28 13:25:35 +0100

    documentation: default parameters | array destructuring

--- es6.js
+++ es6.js
@@ -5,20 +5,22 @@ let age = 20;
 const name = "Doe";
 
 // arrow functions
-const array = [1, 2, 3];
-
-
+// const array = [1, 2, 3];
 
 // template string
 const greeting = (name) => {
     const string = 'Hello, ' + name + ' !';
     const templateString = `Hello, ${2 + 11} !`;
 }
-
 greeting('John')
+
 // default parameters
+const add = (a = 0, b = 2) => console.log(a + b);
 
 // array destructuring
+const array = [1, 2, 3];
+const [ first, second, third ] = array;
+
 
 // object destructuring
 
